0528993852fca6579c87c4e19df5d44a507e637e
olivcabl <ocadena@sura.com.co>
2019-02-08 14:32:12 -0500

    ajuste libreria

--- slackNotifier.groovy
+++ slackNotifier.groovy
@@ -1,22 +1,7 @@
-final TEAMDOMAIN = "negocio-eps"
-final CHANNEL = "#pipeline-as-code"
-final TOKEN = "kXvuJfAZwudfk6lHSxvduEaY"
 
 def call(String buildResult) {
 
-    println buildResult
+    slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
+              color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful " buildResult
 
-  if ( buildResult == "SUCCESS" ) {
-    slackSend teamDomain: println TEAMDOMAIN, channel: CHANNEL, token: TOKEN,
-              color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
 }
