1089306d01ba4111b9ea3ad04706cb48b0712b51
olivcabl <ocadena@sura.com.co>
2019-02-08 15:11:02 -0500

    version final libreria compartida

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,9 +1,20 @@
 
 def call(String buildResult) {
 
-  println buildResult
-
-  slackSend teamDomain: 'negocio-eps', channel: "${params.channel}", token: "${params.token}",
-              color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  
+  if ( buildResult == "SUCCESS" ) {
+    slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
+              color: "good", message: "${currentBuild.fullDisplayName} completado con exito"
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
+              color: "danger", message: "${currentBuild.fullDisplayName} esta Fallando"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
+              color: "warning", message: "${currentBuild.fullDisplayName} esta inestable"
+  }
+  else {
+    slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
+              color: "danger", message: "${currentBuild.fullDisplayName} el resultado es inesperado"	
+  }
 }
