311500c04d09e251633ee1ce565d272df1d0d0d6
olivcabl <ocadena@sura.com.co>
2019-02-08 15:30:21 -0500

    version final libreria compartida 2

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -3,18 +3,18 @@ def call(String buildResult) {
 
   if ( buildResult == "SUCCESS" ) {
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
-              color: "good", message: "${currentBuild.fullDisplayName} completado con exito"
+              color: "good", message: "${currentBuild.fullDisplayName} completado con exito ${currentBuild}"
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
-              color: "danger", message: "${currentBuild.fullDisplayName} esta Fallando"
+              color: "danger", message: "${currentBuild.fullDisplayName} esta Fallando ${currentBuild.absoluteUrl}"
   }
   else if( buildResult == "UNSTABLE" ) { 
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
-              color: "warning", message: "${currentBuild.fullDisplayName} esta inestable"
+              color: "warning", message: "${currentBuild.fullDisplayName} esta inestable ${currentBuild.absoluteUrl}"
   }
   else {
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
-              color: "danger", message: "${currentBuild.fullDisplayName} el resultado es inesperado"	
+              color: "danger", message: "${currentBuild.fullDisplayName} el resultado es inesperado ${currentBuild.absoluteUrl}"	
   }
 }
