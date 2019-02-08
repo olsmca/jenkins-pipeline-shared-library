0961f9fb7e156be464f705b0468c91e961c21bbc
olivcabl <ocadena@sura.com.co>
2019-02-08 14:43:12 -0500

    probando shared libraries notificacion slack

--- slackNotifier.groovy
+++ slackNotifier.groovy
@@ -1,17 +1,7 @@
 
 def call(String buildResult) {
 
-  if ( buildResult == "SUCCESS" ) {
     slackSend teamDomain: 'negocio-eps', channel: '#pipeline-as-code', token: 'kXvuJfAZwudfk6lHSxvduEaY',
               color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-  }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
+  
 }
