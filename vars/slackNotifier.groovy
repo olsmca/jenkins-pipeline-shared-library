b6cb0f39308663e92b1c3f3c7f7828f40800504e
olsmca <36636764+olsmca@users.noreply.github.com>
2019-02-10 10:54:35 -0500

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,15 +1,15 @@
 
-def call(String buildResult) {
+def call(String currentResult) {
 
-  if ( buildResult == "SUCCESS" ) {
+  if ( currentResult == "SUCCESS" ) {
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
               color: "good", message: "${currentBuild.fullDisplayName} completado con exito"
   }
-  else if( buildResult == "FAILURE" ) { 
+  else if( currentResult == "FAILURE" ) { 
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
               color: "danger", message: "${currentBuild.fullDisplayName} esta Fallando ${currentBuild.absoluteUrl}"
   }
-  else if( buildResult == "UNSTABLE" ) { 
+  else if( currentResult == "UNSTABLE" ) { 
     slackSend teamDomain: "${params.teamDomain}", channel: "${params.channel}", token: "${params.token}",
               color: "warning", message: "${currentBuild.fullDisplayName} esta inestable ${currentBuild.absoluteUrl}"
   }
